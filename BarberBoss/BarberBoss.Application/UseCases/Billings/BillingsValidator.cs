217a958842d88b1e0bd5ca307245197ed26188af
Theo Rondon <theoprondon@hotmail.com>
2026-05-07 23:19:36 -0300

    feat: Criando o tratamento de erro para toda a API, com mensagens em arquivo resource e FluentValidation

---
+++ BarberBoss/BarberBoss.Application/UseCases/Billings/BillingsValidator.cs
@@ -0,0 +1,56 @@
+﻿using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
+using BarberBoss.Exception;
+using FluentValidation;
+
+namespace BarberBoss.Application.UseCases.Billings;
+public class BillingsValidator : AbstractValidator<RequestBillingsJson>
+{
+    public BillingsValidator()
+    {
+        // usando fluentValidation para validar os campos do RequestBillingsJson, garantindo que os dados estejam corretos antes de serem processados
+        RuleFor(billings => billings.Date)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.DATE_REQUIRED);
+
+        RuleFor(billings => billings.BarberName)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.BARBER_NAME_REQUIRED)
+            .Length(2, 80)
+            .WithMessage(ResourceErrorMessages.BARBER_NAME_LENGTH);
+                
+        RuleFor(billings => billings.ClientName)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.CLIENT_NAME_REQUIRED)
+            .Length(2, 120)
+            .WithMessage(ResourceErrorMessages.CLIENT_NAME_LENGTH);
+                
+        RuleFor(billings => billings.ServiceName)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.SERVICE_NAME_REQUIRED)
+            .Length(2, 120)
+            .WithMessage(ResourceErrorMessages.SERVICE_NAME_LENGTH);
+                
+        RuleFor(billings => billings.Amount)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_OR_EQUAL_ZERO);
+
+        RuleFor(billings => billings.Amount)
+            .Equal(0)
+            .When(billings => billings.Status == Status.Cancelado)
+            .WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_ZERO_WHEN_CANCELLED);
+                
+        RuleFor(billings => billings.PaymentMethod)
+            .IsInEnum()
+            .WithMessage(ResourceErrorMessages.PAYMENT_METHOD_INVALID);
+                
+        RuleFor(billings => billings.Status)
+            .IsInEnum()
+            .WithMessage(ResourceErrorMessages.STATUS_INVALID);
+                
+        RuleFor(billings => billings.Notes)
+            .MaximumLength(500)
+            .WithMessage(ResourceErrorMessages.NOTES_MAX_LENGTH)
+            .When(billings => billings.Notes is not null);
+    }
+}
--- BarberBoss/BarberBoss.Application/UseCases/Billings/Register/RegisterBillingsUseCase.cs
+++ BarberBoss/BarberBoss.Application/UseCases/Billings/Register/RegisterBillingsUseCase.cs
@@ -4,6 +4,7 @@ using BarberBoss.Communication.Responses;
 using BarberBoss.Domain.Entities;
 using BarberBoss.Domain.Repositories;
 using BarberBoss.Domain.Repositories.Billings;
+using BarberBoss.Exception.ExceptionBase;
 
 namespace BarberBoss.Application.UseCases.Billings.Register;
 public class RegisterBillingsUseCase : IRegisterBillingsUseCase
@@ -21,7 +22,7 @@ public class RegisterBillingsUseCase : IRegisterBillingsUseCase
 
     public async Task<ResponseRegisteredBillingsJson> Execute(RequestBillingsJson request)
     {
-        //Validar request
+        Validate(request);
 
         var entity = _mapper.Map<Billing>(request);
 
@@ -30,5 +31,19 @@ public class RegisterBillingsUseCase : IRegisterBillingsUseCase
         await _unitOfWork.Commit();
 
         return _mapper.Map<ResponseRegisteredBillingsJson>(entity);
+    }
+
+    private void Validate(RequestBillingsJson request)
+    {
+        var validator = new BillingsValidator();
+
+        var result = validator.Validate(request);
+
+        if(result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
     }
 }
